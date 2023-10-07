4886165a716cce24866e4e01ed571ffe7d0a73e7
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 23:37:43 +0900

    あとは記事部分作る

--- js/news-common.js
+++ js/news-common.js
@@ -19,11 +19,19 @@ fetch('/data/news-tag-setting.json')
                 data.forEach(newsItem => {
                     const newsItemElement = document.createElement('div');
                     newsItemElement.classList.add('news-item');
+                    
+                    const newsItemAnchorElement = document.createElement('a');
+                    if (newsItem.article != undefined) {
+                        newsItemAnchorElement.href = "/news/article/?id=" + String(newsItem.id);
+                        newsItemElement.appendChild(newsItemAnchorElement);
+                    }
+
+                    const newsItemTxtContainerElement = newsItem.article != undefined ? newsItemAnchorElement : newsItemElement;
 
                     const dateElement = document.createElement('div');
                     dateElement.classList.add('date');
                     dateElement.textContent = newsItem.date;
-                    newsItemElement.appendChild(dateElement);
+                    newsItemTxtContainerElement.appendChild(dateElement);
 
                     const labelContainerElement = document.createElement('div');
                     labelContainerElement.classList.add('tag');
@@ -36,12 +44,12 @@ fetch('/data/news-tag-setting.json')
                         labelElement.style.backgroundColor = labelSetting.color;
                         labelContainerElement.appendChild(labelElement);
                     });
-                    newsItemElement.appendChild(labelContainerElement);
+                    newsItemTxtContainerElement.appendChild(labelContainerElement);
 
                     const titleElement = document.createElement('div');
                     titleElement.classList.add('title');
                     titleElement.textContent = newsItem.title;
-                    newsItemElement.appendChild(titleElement);
+                    newsItemTxtContainerElement.appendChild(titleElement);
 
                     newsListContainer.appendChild(newsItemElement);
                 });
