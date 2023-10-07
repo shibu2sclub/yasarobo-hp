8d8827249ca119d1a501a8f15358e81c606dc8fe
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 21:45:48 +0900

    nav-menuをほかのページから押しても誤作動しないように・ほかのページに遷移できるように

--- js/common.js
+++ js/common.js
@@ -62,34 +62,37 @@ const generateLogo = generateNavMenu.then((obj) => {
 const smoothScroll = generateLogo.then((obj) => {
     return new Promise ((resolve, reject) => {
         // Select all links with hashes
-        const links = document.querySelectorAll('a[href^="#"]');
+        const links = document.querySelectorAll('a[href^="/#"]');
 
-        // Loop through each link and add a click event listener
-        links.forEach(link => {
-            link.addEventListener('click', function(e) {
-                // Prevent default behavior
-                e.preventDefault();
+        // Check if the current path is root（rootでのみリンクのページ内スクロール遷移が存在）
+        if (location.pathname == '/') {
+            // Loop through each link and add a click event listener
+            links.forEach(link => {
+                link.addEventListener('click', function(e) {
+                    // Prevent default behavior
+                    e.preventDefault();
 
-                // Get the target element's ID
-                const targetId = this.getAttribute('href').slice(1);
-                // Get the target element
-                const targetElement = document.getElementById(targetId);
+                    // Get the target element's ID
+                    const targetId = this.getAttribute('href').slice(2);
+                    // Get the target element
+                    const targetElement = document.getElementById(targetId);
 
-                const navHeaderElement = document.getElementById('nav-head');
-                const navMenuElement = document.getElementById('nav-menu');
+                    const navHeaderElement = document.getElementById('nav-head');
+                    const navMenuElement = document.getElementById('nav-menu');
 
-                // Calculate the distance to scroll
-                // If the nav header is fixed, we need to subtract its height from the distance to scroll
-                console.log(getComputedStyle(navMenuElement).zIndex);
-                const positionToScroll = (targetElement != null ? window.scrollY + targetElement.getBoundingClientRect().top : 0) - (getComputedStyle(navMenuElement).zIndex == 20000 ? navHeaderElement.offsetHeight : 0);
+                    // Calculate the distance to scroll
+                    // If the nav header is fixed, we need to subtract its height from the distance to scroll
+                    console.log(getComputedStyle(navMenuElement).zIndex);
+                    const positionToScroll = (targetElement != null ? window.scrollY + targetElement.getBoundingClientRect().top : 0) - (getComputedStyle(navMenuElement).zIndex == 20000 ? navHeaderElement.offsetHeight : 0);
 
-                // Scroll smoothly to the target element
-                window.scrollTo({
-                    top: positionToScroll >= 0 ? positionToScroll : 0,
-                    behavior: 'smooth'
+                    // Scroll smoothly to the target element
+                    window.scrollTo({
+                        top: positionToScroll >= 0 ? positionToScroll : 0,
+                        behavior: 'smooth'
+                    });
                 });
             });
-        });
+        }
         resolve();
     });
 });
