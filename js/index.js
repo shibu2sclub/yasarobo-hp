420581f8114070bd3c335babc5c876cab588166b
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 18:55:05 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-// Select all links with hashes
-const links = document.querySelectorAll('a[href^="#"]');
-
-// Loop through each link and add a click event listener
-links.forEach(link => {
-    link.addEventListener('click', function(e) {
-        // Prevent default behavior
-        e.preventDefault();
-
-        // Get the target element's ID
-        const targetId = this.getAttribute('href').slice(1);
-        // Get the target element
-        const targetElement = document.getElementById(targetId);
-
-        const navHeaderElement = document.getElementById('nav-head');
-        const navMenuElement = document.getElementById('nav-menu');
-
-        // Calculate the distance to scroll
-        // If the nav header is fixed, we need to subtract its height from the distance to scroll
-        console.log(getComputedStyle(navMenuElement).zIndex);
-        const positionToScroll = (targetElement != null ? window.scrollY + targetElement.getBoundingClientRect().top : 0) - (getComputedStyle(navMenuElement).zIndex == 20000 ? navHeaderElement.offsetHeight : 0);
-
-        // Scroll smoothly to the target element
-        window.scrollTo({
-            top: positionToScroll >= 0 ? positionToScroll : 0,
-            behavior: 'smooth'
-        });
-    });
-});
-
 const keyVisualVideoElement = document.getElementById('key-visual-player');
 window.addEventListener('load', videoPlayerSwitch);
 window.addEventListener('resize', videoPlayerSwitch);
