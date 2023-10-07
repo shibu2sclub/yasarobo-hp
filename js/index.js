137efadf31f38fdf1993692acfcf6ba72445cdec
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 14:33:43 +0900

    indexのナビヘッダー挙動変更

--- js/index.js
+++ js/index.js
@@ -38,3 +38,16 @@ function videoPlayerSwitch() {
         keyVisualVideoElement.setAttribute('src', 'https://www.youtube.com/embed/cW514x4bg20?start=93&si=C_KkbHkAyLTeIPM_&controls=0&autoplay=1&mute=1&loop=1&playlist=cW514x4bg20');
     }
 }
+
+const keyVisualElement = document.getElementById('key-visual');
+window.addEventListener('load', navHeaderBGColorSwitch);
+window.addEventListener('scroll', navHeaderBGColorSwitch);
+
+function navHeaderBGColorSwitch() {
+    if (keyVisualElement.getBoundingClientRect().bottom < 100) {
+        document.getElementById('nav-head').classList.add('scrolled');
+    }
+    else {
+        document.getElementById('nav-head').classList.remove('scrolled');
+    }
+}
