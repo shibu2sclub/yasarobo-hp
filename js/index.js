aa3ca49533ff6207adb808bc2c310cf61d26de5d
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 12:20:47 +0900

    スマホでは動画をロードしない

--- js/index.js
+++ js/index.js
@@ -27,3 +27,16 @@ links.forEach(link => {
         });
     });
 });
+
+const keyVisualVideoElement = document.getElementById('key-visual-player');
+window.addEventListener('load', videoPlayerSwitch);
+window.addEventListener('resize', videoPlayerSwitch);
+
+function videoPlayerSwitch() {
+    if (keyVisualVideoElement.style.display == "none") {
+        keyVisualVideoElement.setAttribute('src', '');
+    }
+    else {
+        keyVisualVideoElement.setAttribute('src', 'https://www.youtube.com/embed/cW514x4bg20?start=93&si=C_KkbHkAyLTeIPM_&controls=0&autoplay=1&mute=1&loop=1&playlist=cW514x4bg20');
+    }
+}
