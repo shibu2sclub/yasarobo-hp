894ab321c9ff82dad74a8d920d21304a3ce6b3df
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-07 15:26:47 +0900

    メニューにおけるモバイル版判定

--- js/index.js
+++ js/index.js
@@ -13,10 +13,12 @@ links.forEach(link => {
         const targetElement = document.getElementById(targetId);
 
         const navHeaderElement = document.getElementById('nav-head');
+        const navMenuElement = document.getElementById('nav-menu');
 
         // Calculate the distance to scroll
         // If the nav header is fixed, we need to subtract its height from the distance to scroll
-        const positionToScroll = (targetElement != null ? window.scrollY + targetElement.getBoundingClientRect().top : 0) - (getComputedStyle(navHeaderElement).position == 'fixed' ? navHeaderElement.offsetHeight : 0);
+        console.log(getComputedStyle(navMenuElement).zIndex);
+        const positionToScroll = (targetElement != null ? window.scrollY + targetElement.getBoundingClientRect().top : 0) - (getComputedStyle(navMenuElement).zIndex == 20000 ? navHeaderElement.offsetHeight : 0);
 
         // Scroll smoothly to the target element
         window.scrollTo({
