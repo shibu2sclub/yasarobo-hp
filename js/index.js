7fcb0cde60478fa94b7934cfb14d29f542bcbc4c
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-09 23:16:30 +0900

    途中

--- js/index.js
+++ js/index.js
@@ -23,3 +23,40 @@ function navHeaderBGColorSwitch() {
         document.getElementById('nav-head').classList.remove('scrolled');
     }
 }
+
+const slideshowElement = document.getElementById('key-visual-img-slideshow');
+const generateSlideshow = new Promise((resolve, reject) => {
+    fetch('/data/index-slideshow.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const imgElement = document.createElement('img');
+                imgElement.setAttribute('src', element.img);
+                const captionElement = document.createElement('span');
+                captionElement.innerHTML = element.caption;
+                slideshowElement.appendChild(imgElement);
+                slideshowElement.appendChild(captionElement);
+            });
+        })
+        .then(() => {
+            const slideshowImgElementsArray = Array.from(slideshowElement.getElementsByTagName('img'));
+            const slideshowCaptionElementsArray = Array.from(slideshowElement.getElementsByTagName('span'));
+            let currentImage = 0
+            setInterval(() => {
+                currentImage += 1
+                if (currentImage >= slideshowImgElementsArray.length) {
+                    currentImage = 0
+                }
+                slideshowImgElementsArray[currentImage - 1 >= 0 ? currentImage - 1 : slideshowImgElementsArray.length - 1].classList.remove('show');
+                slideshowImgElementsArray[currentImage].classList.remove('after');
+                slideshowImgElementsArray[currentImage].classList.add('show');
+                setTimeout(6000);
+                slideshowImgElementsArray[currentImage + 1 <= slideshowImgElementsArray.length - 1 ? currentImage + 1 : 0].classList.add('show');
+                slideshowImgElementsArray[currentImage + 1 <= slideshowImgElementsArray.length - 1 ? currentImage + 1 : 0].classList.add('after');
+            }, 7000)
+        })
+        .then(data => {
+            resolve();
+        })
+        .catch(error => console.error(error));
+});
