d47d70185806b512518a6209f4e97d1850ead263
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-24 01:23:44 +0900

    urlparamで解決する

--- js/topic-path-load-year.js
+++ js/topic-path-load-year.js
@@ -1,38 +1,31 @@
 const judgeYearPastOrLatest = navBGOverlayUpdate.then(() => {
     return new Promise ((resolve, reject) => {
-        fetch('/data/common.json')
-            .then(response => response.json())
-            .then(data => {
-                const topicPathListLatestElement = document.querySelectorAll('.topic-path.latest-contest')[0];
-                const topicPathListPastElement = document.querySelectorAll('.topic-path.past-contests')[0];
-                const pageYearParam = getParam('y');
-                // 過去大会
-                if (pageYearParam != null && pageYearParam != siteYear) {
-                    topicPathListPastElement.style.display = 'flex';
-                    const newsLinksAffectYear = topicPathListPastElement.getElementsByClassName("news-link-affect-year")[0];
-                    if (newsLinksAffectYear != undefined) newsLinksAffectYear.setAttribute('href', `/news/?y=${pageYearParam}`);
+        const topicPathListLatestElement = document.querySelectorAll('.topic-path.latest-contest')[0];
+        const topicPathListPastElement = document.querySelectorAll('.topic-path.past-contests')[0];
+        const pageYearParam = getParam('y');
+        // 過去大会
+        if (pageYearParam != null && pageYearParam != siteYear) {
+            topicPathListPastElement.style.display = 'flex';
+            const newsLinksAffectYear = topicPathListPastElement.getElementsByClassName("news-link-affect-year")[0];
+            if (newsLinksAffectYear != undefined) newsLinksAffectYear.setAttribute('href', `/news/?y=${pageYearParam}`);
 
-                    const recordLinksAffectYear = topicPathListPastElement.getElementsByClassName("record-link-affect-year")[0];
-                    if (recordLinksAffectYear != undefined) recordLinksAffectYear.setAttribute('href', `/record/?y=${pageYearParam}`);
+            const recordLinksAffectYear = topicPathListPastElement.getElementsByClassName("record-link-affect-year")[0];
+            if (recordLinksAffectYear != undefined) recordLinksAffectYear.setAttribute('href', `/record/?y=${pageYearParam}`);
 
-                    const addYearArray = Array.from(document.getElementsByClassName("add-year"));
-                    addYearArray.forEach(addYear => {
-                        addYear.innerText += `（${pageYearParam}年度）`;
-                    });
+            const addYearArray = Array.from(document.getElementsByClassName("add-year"));
+            addYearArray.forEach(addYear => {
+                addYear.innerText += `（${pageYearParam}年度）`;
+            });
 
-                    const addYearEnArray = Array.from(document.getElementsByClassName("add-year-en"));
-                    addYearEnArray.forEach(adYearEn => {
-                        addYearEn.innerText += ` (${pageYearParam})`;
-                    });
-                }
-                // 最新大会
-                else {
-                    topicPathListLatestElement.style.display = 'flex';
-                }
-            })
-            .then(() => {
-                resolve();
-            })
-            .catch(error => console.error(error));
+            const addYearEnArray = Array.from(document.getElementsByClassName("add-year-en"));
+            addYearEnArray.forEach(adYearEn => {
+                addYearEn.innerText += ` (${pageYearParam})`;
+            });
+        }
+        // 最新大会
+        else {
+            topicPathListLatestElement.style.display = 'flex';
+        }
+        resolve();
     });
 });
