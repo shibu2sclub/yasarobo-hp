983c6a7ee2602ee390594c30fc2f3182416385e4
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-23 20:57:38 +0900

    バックアップ

---
+++ js/record-calculate.js
@@ -0,0 +1,6 @@
+/* setting: 連想配列でrecord-setting.json内のcourseSetting.jsonを全コース分そのまま。 */
+/* robotID: ゼッケン番号。コース判定もこれで行う。 */
+/* pointString: 得点の文字列。配列ではなく各試技1回の文字列単体を。 */
+function calculateScore(setting, robotID, pointString) {
+    
+}
