976809c3df619d0f98cbb48d9d73d7be39c706ce
Makoto Yamashita <makochan12.9@gmail.com>
2023-10-25 21:33:58 +0900

    integrate JSs

--- js/common.js
+++ js/common.js
@@ -1,3 +1,37 @@
+// Judge the browser.
+// Return: The browser name.
+
+function judgeBrowser() {
+    var ua = navigator.userAgent;
+    if (ua.indexOf("MSIE") > 0 || ua.indexOf("Trident") > 0) {
+        return "IE";
+    } else if (ua.indexOf("Firefox") > 0) {
+        return "Firefox";
+    } else if (ua.indexOf("Chrome") > 0) {
+        return "Chrome";
+    } else if (ua.indexOf("Chromium") > 0) {
+        return "Chromium";
+    } else if (ua.indexOf("Safari") > 0) {
+        return "Safari";
+    } else if (ua.indexOf("Opera") > 0) {
+        return "Opera";
+    } else if (ua.indexOf("OPR") > 0) {
+        return "Blink Opera";
+    } else {
+        return "Unknown";
+    }
+}
+
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 const allWrapperElement = document.getElementById('all-wrapper');
 const containerElement = document.getElementById('container');
 
